284bbf0444e31b871513ce388771976380f86d29
Saanchita123 <sanchitad648@gmail.com>
2026-01-10 13:01:14 +0530

    21th commit -ui changes

--- src/components/pricing/pricing.tsx
+++ src/components/pricing/pricing.tsx
@@ -76,7 +76,7 @@ const PricingPage: React.FC = () => {
                 plan.highlighted
                   ? "border-primary shadow bg-gradient-to-b from-white to-[#F0F2FF]"
                   : "border-inputBorder bg-white"
-              } overflow-hidden flex flex-col`}
+              } overflow-hidden flex flex-col  max-w-sm min-h-[500px]`}
             >
               <div className="p-6 pb-4">
                 <h2
@@ -88,18 +88,18 @@ const PricingPage: React.FC = () => {
                 >
                   {plan.name}
                 </h2>
-                <div className="flex items-baseline mb-2">
-                  <span className="text-4xl font-bold">₹{plan.price}</span>
+                <div className="flex items-baseline mb-2 ">
+                  <span className="text-4xl font-medium ">₹{plan.price}</span>
                   <span className="ml-2 text-input font-light text-sm">
                     /{plan.period}
                   </span>
                 </div>
-                <p className="text-sm text-input font-light mb-6">
+                <p className="text-sm text-input font-light mb-6 ">
                   {plan.description}
                 </p>
-
+                <hr className="py-2 border-dashed" />
                 <button
-                  className={`w-full py-3 px-4 rounded-[10px] font-medium transition-colors ${
+                  className={`w-full py-3 px-4 rounded-[10px] font-light transition-colors  ${
                     plan.highlighted
                       ? "bg-primary text-white"
                       : "bg-black  text-white"
@@ -110,7 +110,7 @@ const PricingPage: React.FC = () => {
               </div>
 
               <div className="px-6 pb-6 flex-grow">
-                <ul className="space-y-3">
+                <ul className="space-y-5">
                   {plan.features.map((feature, index) => (
                     <li key={index} className="flex items-start">
                       <div className="w-5 h-5 mr-3 flex-shrink-0">
