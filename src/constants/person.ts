b88ebbddaba6c91716a0cf01592ddd48545e5d68
Saanchita123 <sanchitad648@gmail.com>
2026-01-12 11:37:11 +0530

    27th commit

--- src/constants/person.ts
+++ src/constants/person.ts
@@ -3,6 +3,5 @@ export const personImages = [
   { title: "person-2", url: "/testimonials/2.png" },
   { title: "person-3", url: "/testimonials/3.png" },
   { title: "person-4", url: "/testimonials/4.png" },
-  { title: "person-5", url: "/testimonials/5.png" },
   { title: "person-6", url: "/testimonials/6.png" },
 ];
